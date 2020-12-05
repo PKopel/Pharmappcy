2913e7d5cf43e510004cffe3446d6477388e8230
Konrad Przewłoka <305405@student.agh.edu.pl>
2020-12-05 22:41:00 +0100

    Added Employee DAO

--- src/main/java/wt/muppety/dao/EmployeeDao.java
+++ src/main/java/wt/muppety/dao/EmployeeDao.java
@@ -5,14 +5,26 @@ import wt.muppety.model.Employee;
 import java.util.Optional;
 
 public class EmployeeDao extends BaseDao<Employee> {
-    public Optional<Employee> create(String firstname){
-        Employee employee = new Employee();
+    public Optional<Employee> create(String firstname, String lastname, String position, String login, String password){
+        Employee employee = new Employee(firstname,lastname,position,login,password);
         try{
             this.save(employee);
+            return findById(employee.getId());
         }catch (Exception e){
             e.printStackTrace();
-            return Optional.empty();
         }
-        return Optional.of(employee);
+        return Optional.empty();
+    }
+
+    public Optional<Employee> findById(final int indexNumber) {
+        try{
+            Employee employee = currentSession().createQuery("SELECT c FROM Employee c WHERE c.id = :id", Employee.class)
+                    .setParameter("id", indexNumber).getSingleResult();
+            return Optional.of(employee);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return Optional.empty();
     }
 }
