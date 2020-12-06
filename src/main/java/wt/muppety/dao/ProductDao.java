8b81cb44cfa72d580228c9f188854b9f49120a5c
Konrad Przewłoka <305405@student.agh.edu.pl>
2020-12-06 11:32:11 +0100

    Added Transaction DAO

--- src/main/java/wt/muppety/dao/ProductDao.java
+++ src/main/java/wt/muppety/dao/ProductDao.java
@@ -8,27 +8,28 @@ import wt.muppety.model.Supplier;
 import java.util.Optional;
 
 public class ProductDao extends BaseDao<Product> {
-    public Optional<Product> create(Supplier supplier, String name, double unitPrice, boolean onPrescription, String manufacturer){
+    public Optional<Product> create(Supplier supplier, String name, double unitPrice, boolean onPrescription, String manufacturer) {
         Product product = new Product(supplier, name, unitPrice, onPrescription, manufacturer);
-        try{
+        try {
             this.save(product);
             return findById(product.getId());
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return Optional.empty();
     }
 
     public Optional<Product> findById(final int indexNumber) {
-        try{
+        try {
             Product product = currentSession().createQuery("SELECT c FROM Product c WHERE c.id = :id", Product.class)
                     .setParameter("id", indexNumber).getSingleResult();
             return Optional.of(product);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return Optional.empty();
     }
+
 
     //Methods such as addCategory or changeSupplier were not deemed necessary to implement
     // as this task can easily by acomplished using DAO update method
