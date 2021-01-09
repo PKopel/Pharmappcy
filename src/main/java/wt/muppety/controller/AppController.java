6d3066f3b73d6ae15f07e2ef9a742567df291e6c
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-01-09 16:22:06 +0100

    Adding further styling, refactoring the system responsible for displaying lists

--- src/main/java/wt/muppety/controller/AppController.java
+++ src/main/java/wt/muppety/controller/AppController.java
@@ -15,7 +15,7 @@ import java.io.IOException;
  * Main class responsible for loading GUI layouts.
  */
 public class AppController {
-    private final String pathToResources = "../../../";
+    protected final String pathToResources = "../../../";
 
     private final Stage primaryStage;
 
@@ -49,6 +49,33 @@ public class AppController {
                 System.exit(0);
             });
             primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generic function responsible for displaying a window with layout corresponding to
+     * argument layoutName. Loads layout defined in .fxml file and initializes its controller,
+     * then shows it inside root element.
+     *
+     * @param data       Data required by layout controller
+     * @param layoutName Enum LayoutName associated with .fxml file to display
+     * @param root       Root FXML element in which the layout will be displayed
+     * @param <T>        Type of data required by layout controller
+     */
+
+    public <T> void showPane(T data, LayoutName layoutName, Pane root) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(AppController.class.getResource(pathToResources + layoutName.getPath()));
+            root.getChildren().clear();
+            root.getChildren().add(loader.load());
+
+            AbstractController<T> controller = loader.getController();
+            controller.setAppController(this);
+            controller.setData(data);
 
         } catch (IOException e) {
             e.printStackTrace();
