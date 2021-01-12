bc3c8d2f8e5f07cb03d624fdbd624c4e3cdbf02d
Mexolius <39342674+Mexolius@users.noreply.github.com>
2021-01-12 13:05:47 +0100

    Fixing delete in ProductListController and css around DynamicComboBox

--- src/main/java/wt/muppety/controller/EmployeeListController.java
+++ src/main/java/wt/muppety/controller/EmployeeListController.java
@@ -12,7 +12,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import wt.muppety.authentication.Authenticator;
 import wt.muppety.dao.EmployeeDao;
 import wt.muppety.model.Employee;
+import wt.muppety.model.Product;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static wt.muppety.authentication.Permission.canModerateDB;
@@ -38,8 +40,6 @@ public class EmployeeListController extends AbstractController<ObservableList<Em
     public Button editButton;
     @FXML
     public Button deleteButton;
-    @FXML
-    public Button backButton;
     @FXML
     private TextField filterField;
     private ObservableList<Employee> data = FXCollections.observableArrayList();
@@ -86,11 +86,13 @@ public class EmployeeListController extends AbstractController<ObservableList<Em
     }
 
     public void handleDeleteAction(ActionEvent event) {
-        Employee employee = employeeTable.getSelectionModel().getSelectedItem();
         EmployeeDao employeeDao = new EmployeeDao();
-        boolean deleted = employeeDao.deleteById(Employee.class, employee.getId());
-        if (!deleted) System.out.println("Error while deleting " + employee);
-        else data.removeAll(employeeTable.getSelectionModel().getSelectedItems());
+        boolean deleted;
+        for (Employee employee : new ArrayList<>(employeeTable.getSelectionModel().getSelectedItems())) {
+            deleted =employeeDao.deleteById(Employee .class,employee.getId());
+            if (!deleted) System.out.println("Error while deleting " + employee);
+        }
+        data.removeAll(employeeTable.getSelectionModel().getSelectedItems());
     }
 
     public void handleEditAction(ActionEvent event) {
